84640a4da890190383d6efb5c84281bf258e06b1
David Blažek <david.blazek@ambico.cz>
2016-03-26 02:29:08 +0100

    Added STL compatibility

--- tree/trie.hpp
+++ tree/trie.hpp
@@ -1,9 +1,14 @@
 #ifndef _trie_hpp_1337420
 #define _trie_hpp_1337420
 
-#include <string>
-
 #include "../ztl_base.hpp"
+
+#ifdef __stl_compatibility
+	#include <string>
+	#include <vector>
+#endif
+
+#include "../container/string.hpp"
 #include "../container/vector.hpp"
 #include "../container/map.hpp"
 
@@ -13,10 +18,10 @@ __ztl_namespace_start
 		char m_value;
 		bool m_end;
 
-		trie_node() : m_value(NULL), m_end(false), m_nodes(map<char, trie_node*, __stl::less<char>>()) { }
-		trie_node(char _v) : m_value(_v), m_end(false), m_nodes(map<char, trie_node*, __stl::less<char>>()) {	}
+		trie_node() : m_value(NULL), m_end(false), m_nodes(map<char, trie_node*>()) { }
+		trie_node(char _v) : m_value(_v), m_end(false), m_nodes(map<char, trie_node*>()) {	}
 
-		trie_node* operator[](char c) { return m_nodes.find(c) == m_nodes.end() ? NULL : m_nodes[c]; }
+		trie_node* operator[](char c) { return  m_nodes[c]; }
 		
 		inline trie_node* add(char c) {
 			if (m_nodes.find(c) == m_nodes.end()) 
@@ -42,17 +47,58 @@ __ztl_namespace_start
 		trie() : root(new Node()) {}
 		~trie() { delete root; }
 
-		inline bool empty()					{ return this->root->m_nodes.empty(); }
+		inline bool empty()	{ return this->root->m_nodes.empty(); }
 
-		inline void insert(__stl::string s) {
+		inline void insert(string s) {
 			Node* node = root;
 			for (char c : s) node = node->add(c);
 			
 			node->m_end = true;
 		}
 
-		inline void insert(vector<__stl::string> sv) { 
-			for (__stl::string s : sv) insert(s); 
+		inline void insert(vector<string> sv) {
+			for (string s : sv) insert(s);
+		}
+
+		inline Node* find(string s, bool match_words = false) const {
+			Node* node = root;
+			for (char c : s) {
+				if (node == NULL) return NULL;
+				node = (*node)[c];
+			}
+
+			return (match_words) ? ((node->m_end) ? node : NULL) : node;
+		}
+
+		inline bool remove(string s, bool partial_remove = false) {
+			if (find(s, !partial_remove)) return false;
+
+			Node* node = root, *lw2 = root;
+			char lw2_c = s[0];
+
+			for (char c : s) {
+				if (!(node->m_nodes.size() > 1)) {
+					lw2 = node;
+					lw2_c = c;
+				}
+				node = (*node)[c];
+			}
+
+			lw2->remove(lw2_c);
+			return true;
+		}
+
+
+#ifdef __stl_compatibility
+		inline void insert(__stl::string s) {
+			Node* node = root;
+			for (char c : s) node = node->add(c);
+
+			node->m_end = true;
+		}
+
+		inline void insert(__stl::vector<__stl::string> sv) {
+			for (__stl::string s : sv) insert(s);
 		}
 
 		inline Node* find(__stl::string s, bool match_words = false) const {
@@ -82,6 +128,8 @@ __ztl_namespace_start
 			lw2->remove(lw2_c);
 			return true;
 		}
+#endif
+		
 	};
 __ztl_namespace_end
 
