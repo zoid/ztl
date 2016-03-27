7c574d06354d9096016d158a02eb0a2e7012c0ca
David Blažek <david.blazek@ambico.cz>
2016-03-27 16:04:22 +0200

    Some definitions moved to core/define.hpp

--- ztl_base.hpp
+++ ztl_base.hpp
@@ -1,19 +1,8 @@
 #ifndef __ztl_base_loaded
 #define __ztl_base_loaded 1
 
-	#ifndef __ztl_namespace_start
-		#define __ztl_namespace_start  namespace ztl {
-	#endif
+#define __stl_compatibility true
 
-	#ifndef __ztl_namespace_end
-		#define __ztl_namespace_end  }
-	#endif
-
-#ifndef __stl
-#define __stl std
-#endif
-
-//Use stl containers if possible
-#define __stl_compatibility 1
+#include "core\define.hpp"
 
 #endif /* __ztl_base_loaded */
