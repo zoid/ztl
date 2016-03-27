0f2a53cecab41e2b18265d0ebf293bf1b067b9cd
David Blažek <david.blazek@ambico.cz>
2016-03-27 19:30:22 +0200

    Update unordered_map.hpp

--- container/unordered_map.hpp
+++ container/unordered_map.hpp
@@ -17,15 +17,15 @@ private:
 
 public:
 	typedef typename HashTable::key_type				key_type;
-	typedef Value										data_type;
-	typedef Value										mapped_type;
+	typedef Value							data_type;
+	typedef Value							mapped_type;
 	typedef typename HashTable::value_type				value_type;
-	typedef typename HashTable::hasher					hasher;
+	typedef typename HashTable::hasher				hasher;
 	typedef typename HashTable::key_equal				key_equal;
 
 	typedef typename HashTable::size_type				size_type;
 	typedef typename HashTable::difference_type			difference_type;
-	typedef typename HashTable::pointer					pointer;
+	typedef typename HashTable::pointer				pointer;
 	typedef typename HashTable::const_pointer			const_pointer;
 	typedef typename HashTable::reference				reference;
 	typedef typename HashTable::const_reference			const_reference;
@@ -44,26 +44,26 @@ public:
 
 	inline size_type				size() const { return m_table.size(); }
 	inline size_type				max_size() const { return m_table.max_size(); }
-	inline bool						empty() const { return m_table.empty(); }
-	inline void						swap(unordered_map& _hs) { m_table.swap(_hs.m_table); }
+	inline bool					empty() const { return m_table.empty(); }
+	inline void					swap(unordered_map& _hs) { m_table.swap(_hs.m_table); }
 
 
-	iterator						begin() { return m_table.begin(); }
-	iterator						end() { return m_table.end(); }
+	iterator					begin() { return m_table.begin(); }
+	iterator					end() { return m_table.end(); }
 	const_iterator					cbegin() const { return m_table.begin(); }
 	const_iterator					cend() const { return m_table.end(); }
 
 
-	inline pair<iterator, bool>		  insert(const value_type& _val) { return m_table.insert_unique(_val); }
-	inline iterator					        find(const key_type& _key) { return m_table.find(_key); }
-	inline const_iterator			      find(const key_type& _key) const { return m_table.find(_key); }
-	inline Value&					          operator[](const key_type& _key) { return m_table.find_or_insert(make_pair(_key, Value())).second; }
+	inline pair<iterator, bool>		  	insert(const value_type& _val) { return m_table.insert_unique(_val); }
+	inline iterator					find(const key_type& _key) { return m_table.find(_key); }
+	inline const_iterator			      	find(const key_type& _key) const { return m_table.find(_key); }
+	inline Value&					operator[](const key_type& _key) { return m_table.find_or_insert(make_pair(_key, Value())).second; }
 	inline size_type				count(const key_type& _key) const { return m_table.count(_key); }
 	inline size_type				erase(const key_type& _key) { return m_table.erase(_key); }
-	inline void						erase(iterator _it) { m_table.erase(_it); }
-	inline void						erase(iterator _f, iterator _l) { m_table.erase(_f, _l); }
-	inline void						clear() { m_table.clear(); }
-	inline void						resize(size_type _hint) { m_table.resize(_hint); }
+	inline void					erase(iterator _it) { m_table.erase(_it); }
+	inline void					erase(iterator _f, iterator _l) { m_table.erase(_f, _l); }
+	inline void					clear() { m_table.clear(); }
+	inline void					resize(size_type _hint) { m_table.resize(_hint); }
 	inline size_type				bucket_count() const { return m_table.bucket_count(); }
 	inline size_type				max_bucket_count() const { return m_table.max_bucket_count(); }
 	inline size_type				elems_in_bucket(size_type _n) const { return m_table.elems_in_bucket(_n); }
